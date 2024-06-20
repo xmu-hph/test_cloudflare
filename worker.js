f9b6a62822f58495280d438d7aeb0dbc1aa94f42
xmu-hph <155121366+xmu-hph@users.noreply.github.com>
2024-06-20 13:20:18 +0800

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,5 +2,5 @@ addEventListener('fetch', event => {
   const url = new URL(event.request.url);
   url.hostname = 'https://www.google.com'; // 目标服务器地址
   const newRequest = new Request(url, event.request);
-  event.respondWith(await fetch(newRequest));
+  event.respondWith(fetch(newRequest));
 });
